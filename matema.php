3a042689248416b84990f9d9de265d219faf2e56
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-04-09 19:59:22 -0400

    Update matema.php
    melhorando o desine e o código

--- matema.php
+++ matema.php
@@ -1,41 +1,64 @@
-<html>
-  <head>
-    <title>adiciona number</title>
-    <style>
-        input,
-        body,
-        form,
-        #numero {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            font-size: 30px;
-        }
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Adiciona</title>
+  <style>
+    * { margin: 0; box-sizing: border-box; }
 
-        body,
-        #but {
-            background: black;
-            margin: 2px;
-            color: white;
-        }
+    body {
+      height: 100vh;
+      background: black;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: sans-serif;
+    }
 
-        #for,
-        #numero,
-        #but {
-            border-radius: 10px;
-            padding: 10px;
-            border: 3px solid black;
-        }
+    form {
+      background: white;
+      padding: 20px;
+      border-radius: 10px;
+      text-align: center;
+    }
 
-        #for {
-            background: white;
-        }
-    </style>
+    label,input {
+      font-size: 40px;
+      padding: 10px 20px;
+      border-radius: 10px;
+      margin-top: 10px;
+    }
+
+    input[type="number"] {
+      border:2px solid black;
+      text-align: center;
+      -moz-appearance: textfield;
+    }
+
+    input[type="number"]::-webkit-inner-spin-button,
+    input[type="number"]::-webkit-outer-spin-button {
+      -webkit-appearance: none;
+    }
+
+    input[type="number"]:focus {
+      outline: none;
+      border-color: blue;
+      box-shadow: 0 0 5px blue;
+    }
+
+    input[type="submit"] {
+      background: black;
+      color: white;
+      border: none;
+      cursor: pointer;
+    }
+  </style>
 </head>
 <body>
-    <form method="GET" action="soma.php" id="for">
-      <input valor="" name="numero" id="numero" type="number" placeholder="numero"><br>
-      <input type="submit" value="Enter" id="but"></form>
+  <form method="GET" action="soma.php">
+    <label for="number">add a value</label><br><hr>
+    <input id="number" name="number" type="number" placeholder="number"><br>
+    <input type="submit" value="Start">
+  </form>
 </body>
-
 </html>
