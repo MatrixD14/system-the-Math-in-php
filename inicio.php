883e4d16158ae2c681d09abb6cdc4c769f5b72b0
MatrixD14 <166034702+MatrixD14@users.noreply.github.com>
2025-04-09 20:00:28 -0400

    Update inicio.php
    melhoro o desine do button

--- inicio.php
+++ inicio.php
@@ -1,4 +1,9 @@
 <html>
 <head><title>inicio</title></head>
-<body style="display: flex;justify-content: center;align-items: center;background: black;"><a href="matema.php" style="background: white;text-decoration:none;padding:20px;border-radius:10px;font-size:50px;color:black;">clique aqui</a></body>
+<body style="background: black; display: flex;height:100vh; justify-content: center; margin: 0; align-items: center;">
+<div style=" display: flex;flex-direction: column; align-items: center;text-align: center;">
+<h1 style="color:white;font-size:10vh;">don't click the button</h1>
+<a href="matema.php" style="padding: 10px 30px; background: white;text-decoration:none;padding:10px 20px;border-radius:10px;font-size:50px;color:black;">click</a>
+</div>
+</body>
 </html>
